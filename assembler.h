3628b1730f48c3979a5c5487df8366bcf54a6a1d
afik319 <134519780+afik319@users.noreply.github.com>
2023-08-20 12:31:42 +0300

    Update assembler.h

--- assembler.h
+++ assembler.h
@@ -1,9 +1,5 @@
 /*Created by elifu on 02/08/2023.*/
 
-#ifndef ASSEMBLER_OPENU_PROJECT_ASSEMBLER_H
-#define ASSEMBLER_OPENU_PROJECT_ASSEMBLER_H
-
 #include "second_pass.h"
 #define FILE_NAME 255
 void free_ld_structs(line_data** ld_arr[], int ld_arr_size);
-#endif /*ASSEMBLER_OPENU_PROJECT_ASSEMBLER_H*/
